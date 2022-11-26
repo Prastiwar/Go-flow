a21bd1255286459d5c2edc1f85b5168c6d9a9f21
Tomasz Piowczyk <tomasz.piowczyk@gmail.com>
2022-11-26 11:42:03 +0100

    Add interfaces

--- caches/cache.go
+++ caches/cache.go
@@ -1,7 +1,24 @@
-package cashes
+// Package caches provides abstraction for both caching strategies in memory and distributed.
+package caches
 
+import (
+	"context"
+	"time"
+)
+
+// Cache is an interface defining evicting cache contract with individual ttl configuration.
 type Cache interface {
-	Set(ctx context.Context, key interface{}, value interface{}, expireTime time.Duration) error
-	Get(ctx context.Context, key interface{}) ([]byte, error)
-	Delete(ctx context.Context key interface{}) error
+	Set(key string, value interface{}, ttl time.Duration) error
+	Get(key string) ([]byte, error)
+	Delete(key string) error
+	Evict(evict func(key string, data []byte)) error
+}
+
+// DistributedCache is an interface defining cache which can be distributed on server like redis.
+// The different between cache and distributed in interface is accepting context to respect
+// requests cancellation.
+type DistributedCache interface {
+	Set(ctx context.Context, key string, value interface{}, ttl time.Duration) error
+	Get(ctx context.Context, key string) ([]byte, error)
+	Delete(ctx context.Context, key string) error
 }
